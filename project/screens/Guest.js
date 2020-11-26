15de46dc88ec8f21c106da463a3087d14d74f9f8
pingchoy <pingchoy.1@gmail.com>
2020-11-26 12:18:36 +1100

    Revert "fixed merge"
    This reverts commit a3e1051b6553759e751ad418b08748e7752386b7, reversing
    changes made to 40a20352e12370ef382edc85d0d787d6e9f65007.

--- project/screens/Guest.js
+++ project/screens/Guest.js
@@ -1,60 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, Image, SafeAreaView, TextInput } from 'react-native';
+import { View, StyleSheet, Dimensions, Text, TouchableOpacity, Image, SafeAreaView, TextInput } from 'react-native';
 import { GoButton } from '../components/GoButton';
 
-export default function Guest({ navigation }) {
+const dimensions = Dimensions.get('window');
+
+export default function Login({ navigation }) {
   const [username, onChangeUsername] = React.useState('Enter a username');
   const [code, onChangeCode] = React.useState('Enter an event code');
 
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.backButtonView}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image style={styles.backButton} source={require('../assets/backButton.png')} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputBody}
+          placeholder={username}
+          onChangeUsername={text => onChangeUsername(text)}
+          username={username}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputBody}
+          placeholder={code}
+          onChangePassword={text => onChangeCode(text)}
+          code={code}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <GoButton></GoButton>
+      </View>
+
       <StatusBar style="auto" />
-      <Text style={styles.heading}>Sign Up</Text>
-      <TextInput
-        style={styles.usernameBody}
-        placeholder={username}
-        onChangeUsername={text => onChangeUsername(text)}
-        username={username}
-      />
-      <TextInput
-        style={styles.codeBody}
-        placeholder={code}
-        onChangeCode={text => onChangeCode(text)}
-        code={code}
-      />
-      <GoButton></GoButton>
-      <Image style={styles.banner} source={require('../assets/banner.png')} />
+      <Image style={styles.banner} source={require('../assets/bottomBanner.png')} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  banner: {
+    position: 'absolute',
+    height: 112,
+    width: dimensions.width,
+    bottom: 0,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  heading: {
+  backButtonView: {
     position: 'absolute',
-    left: 24,
-    top: 224,
-    //fontFamily: 'Roboto',
+    left: 23,
+    top: 58,
+  },
+  inputView: {
+    width: "100%",
+    justifyContent: 'center',
+    alignItems: "center",
+    marginBottom: 30,
+    height: 55,
+    padding: 20,
+  },
+  heading: {
     fontStyle: 'normal',
     fontWeight: 'bold',
     fontSize: 32,
-    lineHeight: 37,
     color: '#3C3C3C',
-  },
-  usernameBody: {
     position: 'absolute',
-    width: 366,
+    alignSelf: "flex-start",
+    margin: 20,
+  },
+
+  inputBody: {
+    position: 'absolute',
+    paddingLeft: 20,
     height: 55,
-    left: 24,
-    top: 348,
-    paddingLeft: 16,
-    //fontFamily: 'Roboto',
+    width: '100%',
     fontStyle: 'normal',
     fontWeight: 'bold',
     fontSize: 18,
@@ -66,23 +95,30 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
   },
-  codeBody: {
+
+
+  textView: {
     position: 'absolute',
-    width: 263,
-    height: 51,
-    left: 24,
-    top: 414,
-    paddingLeft: 16,
-    //fontFamily: 'Roboto',
-    fontStyle: 'normal',
-    fontWeight: 'bold',
-    fontSize: 18,
-    lineHeight: 21,
-    color: '#3C3C3C',
-    backgroundColor: 'white',
-    borderRightColor: 'black',
-    borderColor: "black",
-    borderWidth: 1,
-    borderRadius: 10,
+    bottom: 0,
+    width: "100%",
+    justifyContent: 'center',
+    alignItems: "center",
+    marginBottom: 50,
+    marginTop: 30,
+    height: 55,
   },
+
+  subheading: {
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 20,
+    color: '#3C3C3C',
+  },
+
+  subLink: {
+    textDecorationLine: 'underline',
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#165F22',
+  }
 });
