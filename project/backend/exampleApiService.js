cfe921ad7b9f0c10069084448e57b0e21abfd80a
Corrade <corrade@hotmail.com>
2020-12-02 02:01:58 +1100

    Cosmetic reformatting

--- project/backend/exampleApiService.js
+++ project/backend/exampleApiService.js
@@ -3,24 +3,32 @@ const API_BASE_URL = 'http://localhost:5000';
 /**
  * Returns the response of the request in json form or throws an error.
  * 
- * Sample usage after importing makeAPIRequest from this file:
+ * Sample usage in an async function in a file that has imported makeAPIRequest
+ * from here:
+ * 
  * try {
  *   await makeAPIRequest('auth/login', 'POST', { email, passsword });
  * } catch (err) {
  *   console.log(err.message);
  * }
  */
-async function makeAPIRequest(endpoint, method, body = {}) {
+async function makeAPIRequest(endpoint, givenMethod, givenBody = {}) {
   const reqOptions = {
     headers: {
-      Authorization: `Bearer ${getToken()}`, // getToken() is some function that retrives the token of the current user, probably from local storage. It's ok if getToken() returns a non-token value as that will be caught by the backend and cause an appropriate error to be thrown from this function. It's also ok if this authorization header is provided when it isn't necessary - it'll just be ignored.
+      Authorization: `Bearer ${getToken()}`,
       'Content-Type': 'application/json',
     },
-    method: method,
+    method: givenMethod,
   };
 
-  if (method !== 'GET' && body !== {}) {
-    reqOptions.body = JSON.stringify(body);
+  // getToken() is some function that retrieves the token of the current user,
+  // probably from local storage. It's ok if getToken() returns a non-token
+  // value as that'll be caught by the backend and cause an appropriate
+  // error to be thrown. It's also ok if this authorization header is provided
+  // when it isn't necessary - it'll just be ignored.
+
+  if (givenMethod !== 'GET' && givenBody !== {}) {
+    reqOptions.body = JSON.stringify(givenBody);
   }
 
   const res = await fetch(`${API_BASE_URL}/${endpoint}`, reqOptions);
